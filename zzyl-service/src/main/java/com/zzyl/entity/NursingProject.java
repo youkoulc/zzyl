34b37730fac1fb4f1ac54b076bf8ccc26ae5d083
wuhuijin <1078063646@qq.com>
2024-07-08 18:18:25 +0800

    feat zzyl-service): 创建老人护理等级和计划的数据传输对象
    - 创建NursingLevelDto和NursingPlanDto类，用于老人护理等级和计划的数据传输。
    - 在NursingProjectDto中添加计划id和项目id的相关字段和注释。
    - 更新NursingElder、NursingLevel、NursingPlan和NursingProject实体类，完善字段和注释。
    - 创建NursingTask类，用于表示老人护理任务。- 更新NursingProjectVo，添加护理项目计划列表和护理等级id的相关字段和注释。
    - 创建NursingLevelVo和NursingPlanVo类，用于老人护理等级和计划的视图对象。
    - 更新NursingTaskVo，添加执行图片和护理项目名称等相关字段和注释。

--- zzyl-service/src/main/java/com/zzyl/entity/NursingProject.java
+++ zzyl-service/src/main/java/com/zzyl/entity/NursingProject.java
@@ -1,63 +1,56 @@
 package com.zzyl.entity;
 
 import com.zzyl.base.BaseEntity;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 /**
- * 实体类：NursingProject（护理项目）
- */
-@Data
+* <p>
+* nursing_project 实体类
+* </p>
+*
+* @author yuhon
+* @since 2024-01-02 11:10:46
+*/
+@Getter
+@Setter
 public class NursingProject extends BaseEntity {
 
     /**
-     * 编号
-     */
-    @ApiModelProperty(value = "编号")
-    private Long id;
-
-    /**
-     * 名称
-     */
-    @ApiModelProperty(value = "名称")
+    * 名称
+    */
     private String name;
 
     /**
-     * 排序号
-     */
-    @ApiModelProperty(value = "排序号")
+    * 排序号
+    */
     private Integer orderNo;
 
     /**
-     * 单位
-     */
-    @ApiModelProperty(value = "单位")
+    * 单位
+    */
     private String unit;
 
     /**
-     * 价格
-     */
-    @ApiModelProperty(value = "价格")
+    * 价格
+    */
     private BigDecimal price;
 
     /**
-     * 图片
-     */
-    @ApiModelProperty(value = "图片")
+    * 图片
+    */
     private String image;
 
     /**
-     * 护理要求
-     */
-    @ApiModelProperty(value = "护理要求")
+    * 护理要求
+    */
     private String nursingRequirement;
 
     /**
-     * 状态（0：禁用，1：启用）
-     */
-    @ApiModelProperty(value = "状态（0：禁用，1：启用）")
+    * 状态（0：禁用，1：启用）
+    */
     private Integer status;
-  
+
 }
